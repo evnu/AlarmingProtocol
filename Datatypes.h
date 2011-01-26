e160f150d4ec7e910e9023951bf7557542d95da5
unknown <mm@.(none)>
2011-01-26 20:10:35 +0100

    messages

--- Datatypes.h
+++ Datatypes.h
@@ -1,22 +1,32 @@
-#ifndef _DATATYPES
-#define _DATATYPES
+#ifndef _DATATYPES_H
+#define _DATATYPES_H
 
 /* Some utilities */
 typedef std::list<RTDS_QueueId> RefList;
 typedef RefList::iterator RefListIt;
 
 /* we need to wrap alarming messages */
-enum MessageMarker {
+enum MessageType {
    ALERT      /* Message tells about suspected alarm */
   ,FALSEALERT /* Message tells about a false alarm */
+  ,REAWAKE    /* Reawake the app layer if it was suspended after a global alarm */
 };
 
-typedef struct {
-  MessageMarker mark;
+/*
+  TransportMessage
+  The following struct describes the payload which the messages broadcast and unicast
+  carry between two process instances of type pTransportSender/pTransportReceiver.
+  Such a message carries the payload from an application layer - Therefore, it is marked
+  with the type MessageType and contains either an integer cnt if it is marked as ALERT,
+  or a RTDS_QueueId appLayer if marked as FALSEALERT. If such a message is marked REAWAKE,
+  the contained payload isn't initialized and must me ignored.
+*/
+typedef struct TransportMessage {
+  MessageType mark;
   
   union payload_t {
-  int cnt;
-  RTDS_QueueId appLayer;
+    int cnt;
+    RTDS_QueueId appLayer;
   } payload;
   
 } TransportMessage;
